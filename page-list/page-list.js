03ab6af86976bd29a4183d76d8d8d2b786fd0db1
differrenz <98097057+differrenz@users.noreply.github.com>
2023-03-28 23:25:01 +0200

    .....

--- page-list/page-list.js
+++ page-list/page-list.js
@@ -47,7 +47,7 @@ export default class PageList extends Page {
                 console.log(image)
 
                 if (image) {
-                    document.getElementById("profileImage").src = image
+                    this._mainElement.getElementById("profileImage").src = image
                 }
             }
 
